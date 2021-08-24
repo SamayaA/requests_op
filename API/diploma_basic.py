117d5439fb4067a070416bbdf0fca188a393e610
SamayaA <asadullaevasamaya@gmail.com>
2021-08-24 21:06:43 +0300

    Fixed for loop for creating file name , added vk_owner_id to class attributes

--- API/diploma_basic.py
+++ API/diploma_basic.py
@@ -1,14 +1,13 @@
 import requests
 from datetime import datetime
 import json
-# импортируем pprint для более комфортного вывода информации
 from pprint import pprint
 
 class YaUploader:
-    def __init__(self, token_yandex: str , token_vk: str):
+    def __init__(self, token_yandex: str , vk_owner_id :str ,  token_vk: str):
         self.token_yandex = token_yandex
         self.token_vk = token_vk
-        self.file_json = list()
+        self.vk_owner_id = vk_owner_id
 
     def upload(self, file: str , file_name : str):
         API_BASE_URL = "https://cloud-api.yandex.net/"
@@ -27,9 +26,10 @@ class YaUploader:
 
     def profile_photos (self) :
         URL = 'https://api.vk.com/method/photos.get'
+        self.file_json = list()
         params = {
-         'owner_id': '560059273',
-         'access_token': self.token_vk,
+         'owner_id' : self.vk_owner_id ,
+         'access_token': self.token_vk ,
          'v':'5.131' ,
          'album_id' : 'profile' , 
          'extended' : '1'
@@ -39,17 +39,17 @@ class YaUploader:
             res_json = res.json()['response']
             for photo in res_json['items'] :
                 self.file_json.append(dict())
-
+                # check same name existence and create the name
                 name_exist = 0
-                if (len(self.file_json) != 1) :
-                    for file in self.file_json :
-                        if file['file_name'] in str(photo['likes']['count']) :
+                if (len(self.file_json) > 1) :
+                    for i in range (len(self.file_json)-1) :
+                        if self.file_json[i]['file_name'] in str(photo['likes']['count']) :
                             self.file_json[-1]['file_name'] = str(photo['likes']['count']) + ' ' + str(datetime.utcfromtimestamp(photo['date']).strftime('%Y-%m-%d '))
                             name_exist = 1
                             break
                 if name_exist != 1 :
-                   self.file_json[-1]['file_name'] = str(photo['likes']['count'])  
-
+                    self.file_json[-1]['file_name'] = str(photo['likes']['count'])  
+                #data of photo
                 self.file_json[-1]['height'] = photo['sizes'][-1]['height']
                 self.file_json[-1]['width'] = photo['sizes'][-1]['width']
                 self.file_json[-1]['type'] = photo['sizes'][-1]['type']
@@ -57,6 +57,7 @@ class YaUploader:
 
                 file = requests.get(photo['sizes'][-1]['url']).content #file to upload to yandex disk
                 self.upload(file , self.file_json[-1]['file_name']) # upload photo to yandex disk
+
         except KeyError :
             print ('Params of https://api.vk.com/method/photos.get request are incorrect')
     
@@ -65,10 +66,10 @@ class YaUploader:
             json.dump(self.file_json, f, ensure_ascii=False)
 
 if __name__ == '__main__':
-    path_to_file = 'Users/Samaya/Desktop/Netology/OOP/requests/Netology'
     token_yandex = ...
     token_vk = ...
-    uploader = YaUploader(token_yandex , token_vk)
+    vk_owner_id = ...
+    uploader = YaUploader(token_yandex , vk_owner_id , token_vk)
     uploader.profile_photos()
     uploader.data_to_json()
 
